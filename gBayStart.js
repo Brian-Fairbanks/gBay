3dad1cb22b462dec485041ca6cfea2c9ff15269b
fruityvegetables <gg.troyer@gmail.com>
2020-04-17 13:20:22 -0500

    started a server/some functions

---
+++ gBayStart.js
@@ -0,0 +1,48 @@
+var mysql = require("mysql");
+var sqlPassword = require("password.js");
+var inquirer = require("inquirer.js");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: sqlPassword,
+  database: "bidding_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    queryAllItems();
+    queryCurrentBids();
+  });
+
+  function queryAllItems() {
+    connection.query("SELECT * FROM items", function(err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].item + " | " + res[i].current-bid);
+      }
+      console.log("-----------------------------------");
+    });
+  }
+
+  function queryCurrentBids() {
+    var query = connection.query("SELECT * FROM items WHERE current-bid=true", function(err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].item + " | " + res[i].current-bid);
+      }
+    });
+  
+    // logs the actual query being run
+    console.log(query.sql);
+    connection.end();
+  }
+
